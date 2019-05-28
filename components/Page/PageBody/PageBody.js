a709068127d1db07c4b617bd0587a0eae59f2442
unknown <kadhirus99@utexas.edu>
2019-05-27 19:20:46 -0500

    Subsection details are passed to Section components now. TODO: might need to make a Subsubsection component

--- components/Page/PageBody/PageBody.js
+++ components/Page/PageBody/PageBody.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import './PageBody.css';
 import Section from './Section/Section'
+import educationData from './Section/educationData.json'
+import collegiateActivitiesData from './Section/collegiateActivitiesData.json'
 
 class PageBody extends React.Component {
 
   render(){
     return(
       <div id="page-body">
-        <Section title="Education" />
-        <Section title="Collegiate Activities" />
+        <Section title="Education" info={educationData}/>
+        <Section title="Collegiate Activities" info={collegiateActivitiesData}/>
       </div>
     )
   }
--- components/Page/PageBody/Section/Section.js
+++ components/Page/PageBody/Section/Section.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import './Section.css';
 import Subsection from './Subsection/Subsection'
-import educationData from './educationData.json'
 
 class Section extends React.Component {
 
   render(){
-    const educationInfo = educationData.map((item) => {
-      return (<Subsection name={item.name} date={item.date} details={item.details} key={item.key}/>)
+
+    const sectionDetails = this.props.info.map((detail) => {
+      return (<Subsection name={detail.name} date={detail.date} details={detail.details}/>)
     })
+
     return(
       <div id="section-container">
         <div id="section-header">{this.props.title.toUpperCase()}</div>
         <div id="subsection-container">
-          {educationInfo}
+          {sectionDetails}
         </div>
       </div>
     )
