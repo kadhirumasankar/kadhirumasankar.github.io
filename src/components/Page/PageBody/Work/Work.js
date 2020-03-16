01c03a5997b71a15cb55e7e5222036b3bbc7e650
Kadhir Umasankar <kadhirus99@utexas.edu>
2020-03-16 14:06:45 -0500

    :pencil2: Changed project information

--- src/components/Page/PageBody/Work/Work.js
+++ src/components/Page/PageBody/Work/Work.js
@@ -1,6 +1,8 @@
 import React from 'react';
+
 import './Work.css';
 import '../About/About.css'
+
 import ProjectCard from './ProjectCard/ProjectCard'
 import projectData from './projectData.json'
 
